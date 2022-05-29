3d2eabac87b6e3fd02be2649788313ab383a4b65
mtabak <mikhail.tabak@raisin.com>
2022-05-30 01:52:44 +0200

    script fix

--- src/main/resources/db/migration/V1__create_url_table.sql
+++ src/main/resources/db/migration/V1__create_url_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE url
     hash VARCHAR(16) NOT NULL,
     origin_url VARCHAR NOT NULL,
     creation_date TIMESTAMP NOT NULL,
-    expirationDate TIMESTAMP NOT NULL,
+    expiration_date TIMESTAMP NOT NULL,
     PRIMARY KEY (hash)
 );
 
